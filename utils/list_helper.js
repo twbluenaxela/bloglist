568eb958a795244457f6139bfce1f149a96a76b3
阿洋 <rednaxela64@outlook.com>
2022-08-04 23:50:31 +0000

    Add top liked post reducer

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,14 +2,25 @@ const dummy = (blogs) => 1;
 
 const totalLikes = (blogList) => blogList.reduce(
   (sum, blog) =>
-//   console.log('----')
-//   console.log("Current sum: ", sum)
-//   console.log('Current blog like count: ', blog.likes)
+  //   console.log('----')
+  //   console.log("Current sum: ", sum)
+  //   console.log('Current blog like count: ', blog.likes)
     sum + blog.likes,
   0,
 );
 
+const favoriteBlog = (blogList) => {
+    const topPost = blogList.reduce((topLikes, blog) => ((topLikes.likes > blog.likes) ? topLikes : blog), 0)
+    return {
+        title: topPost.title,
+        author: topPost.author,
+        likes: topPost.likes,
+    };
+};
+
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
