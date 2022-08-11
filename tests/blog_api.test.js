c2150090f54bbe6c9947e0005f9bcf3d587a14e3
Nick Yu <rednaxela64@outlook.com>
2022-08-10 21:07:50 -0700

    Add likes property validator and test

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,76 +1,79 @@
-const { after } = require('lodash')
-const mongoose = require('mongoose')
-const supertest = require('supertest')
-const helper = require('./test_helper')
+const { after } = require("lodash");
+const mongoose = require("mongoose");
+const supertest = require("supertest");
+const helper = require("./test_helper");
 
-const app = require('../app')
-const api = supertest(app)
+const app = require("../app");
+const api = supertest(app);
 
-const Blog = require('../models/blog')
+const Blog = require("../models/blog");
 
 beforeEach(async () => {
-    await Blog.deleteMany({})
+  await Blog.deleteMany({});
 
-    for(let blog of helper.initialBlogs) {
-        let blogObject = new Blog(blog)
-        await blogObject.save()
-    }
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
+});
 
-})
-
-test('blogs are returned as json', async () => {
-    console.log('entered test')
-    await api
-    .get('/api/blogs')
+test("blogs are returned as json", async () => {
+  console.log("entered test");
+  await api
+    .get("/api/blogs")
     .expect(200)
-    .expect('Content-Type', /application\/json/)
-})
+    .expect("Content-Type", /application\/json/);
+});
 
-test('that the blog id is formatted correctly', async () => {
-    const resultBlogs = await api
-    .get('/api/blogs')
-    console.log(resultBlogs.body)
-    //throwing this code here so I can make sure that if even ONE of them doesn't have the correct formatting, it will fail the test.
-    // resultBlogs.push({
-    //     {
-    //         _id: "5a422a851b54a676234d17f7aaaaaa",
-    //         title: "React patterns",
-    //         author: "Michael Chan",
-    //         url: "https://reactpatterns.com/",
-    //         likes: 7,
-    //         __v: 0
-    //       }
-    // })
+test("that the blog id is formatted correctly", async () => {
+  const resultBlogs = await api.get("/api/blogs");
+  console.log(resultBlogs.body);
+  //throwing this code here so I can make sure that if even ONE of them doesn't have the correct formatting, it will fail the test.
+  // resultBlogs.push({
+  //     {
+  //         _id: "5a422a851b54a676234d17f7aaaaaa",
+  //         title: "React patterns",
+  //         author: "Michael Chan",
+  //         url: "https://reactpatterns.com/",
+  //         likes: 7,
+  //         __v: 0
+  //       }
+  // })
 
-    const blogIds = resultBlogs.body.map(b => expect(b.id).toBeDefined())
+  const blogIds = resultBlogs.body.map((b) => expect(b.id).toBeDefined());
+});
 
-})
+test("if a blog post has been successfully added", async () => {
+  const newBlog = {
+    title: "Hi",
+    author: "Gladwell",
+    url: "https://supaman.com",
+    likes: 9001,
+  };
 
-test('if a blog post has been successfully added', async() => {
-
-    const newBlog = {
-        title: 'Hi',
-        author: 'Gladwell',
-        url: "https://supaman.com",
-        likes: 9001,
-    }
-
-    await api
-    .post('/api/blogs')
+  await api
+    .post("/api/blogs")
     .send(newBlog)
     .expect(201)
-    .expect('Content-Type', /application\/json/)
+    .expect("Content-Type", /application\/json/);
 
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-    const authors = blogsAtEnd.map(b => b.author)
-    expect(authors).toContain(
-        'Gladwell'
-    )
+  const authors = blogsAtEnd.map((b) => b.author);
+  expect(authors).toContain("Gladwell");
+});
 
-})
+test("if like is set to zero if missing like value", async () => {
+    const response = await api.get('/api/blogs')
+    console.log(response.body)
+    const resultBlogs = response.body.map(blog => {
+        //if there is no like property, then it will fail the test. 
+        expect(blog.likes).toBeDefined()
+    } )
+
+});
 
 afterAll(() => {
-    mongoose.connection.close()
-})
+  mongoose.connection.close();
+});
--- tests/test_helper.js
+++ tests/test_helper.js
@@ -38,7 +38,6 @@ const initialBlogs = [
       title: "TDD harms architecture",
       author: "Robert C. Martin",
       url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
-      likes: 0,
       __v: 0
     },
     {
