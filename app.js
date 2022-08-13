5a69faa8f1f4cd822d3a07763ecac80799252419
Nick Yu <rednaxela64@outlook.com>
2022-08-12 19:08:51 -0700

    Add token auth to login

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const logger = require('./utils/logger');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger);
 
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
