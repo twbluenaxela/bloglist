a3f97dedf6065708d25d91a5c0b372776edf7197
Nick Yu <rednaxela64@outlook.com>
2022-08-12 18:54:01 -0700

    populate with user and blog info

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user')
 const logger = require('../utils/logger');
 
 // blogsRouter.get('/', (req, res) => {
@@ -7,21 +8,28 @@ const logger = require('../utils/logger');
 // });
 
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog
+  .find({}).populate('user')
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const { body } = request;
+  const body = request.body
+
+  const user = await User.findById(body.userId)
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id,
   });
 
   if (blog.title && blog.url) {
     const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog);
   } else {
     response.sendStatus(400);
