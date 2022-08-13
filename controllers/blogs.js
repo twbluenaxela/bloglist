05f1569ca5a5f438af432b222b1be461f824cf21
Nick Yu <rednaxela64@outlook.com>
2022-08-12 19:15:05 -0700

    allow only post req with valid token to be sent

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,36 @@
 const blogsRouter = require('express').Router();
+const jwt = require('jsonwebtoken');
 const Blog = require('../models/blog');
-const User = require('../models/user')
+const User = require('../models/user');
 const logger = require('../utils/logger');
 
 // blogsRouter.get('/', (req, res) => {
 //   res.send('<h1>Hi!</h1>');
 // });
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7);
+  }
+  return null;
+};
+
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog
-  .find({}).populate('user')
+    .find({}).populate('user');
   response.json(blogs);
 });
 
 blogsRouter.post('/', async (request, response) => {
-  const body = request.body
+  const { body } = request;
+  const token = getTokenFrom(request);
+  const decodedToken = jwt.verify(token, process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' });
+  }
 
-  const user = await User.findById(body.userId)
+  const user = await User.findById(decodedToken.id);
 
   const blog = new Blog({
     title: body.title,
@@ -28,8 +42,8 @@ blogsRouter.post('/', async (request, response) => {
 
   if (blog.title && blog.url) {
     const savedBlog = await blog.save();
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
     response.status(201).json(savedBlog);
   } else {
     response.sendStatus(400);
@@ -41,20 +55,24 @@ blogsRouter.post('/', async (request, response) => {
 });
 
 blogsRouter.delete('/:id', async (request, response, next) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
-})
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
+});
 
 blogsRouter.put('/:id', async (request, response, next) => {
-
-  const { title, author, url, likes } = request.body
+  const {
+    title, author, url, likes,
+  } = request.body;
 
   const updatedBlog = await Blog.findByIdAndUpdate(
     request.params.id,
-    { title, author, url, likes },
-    { new: true, runValidators: true, context: 'query'})
-  
-  response.json(updatedBlog)
-})
+    {
+      title, author, url, likes,
+    },
+    { new: true, runValidators: true, context: 'query' },
+  );
+
+  response.json(updatedBlog);
+});
 
 module.exports = blogsRouter;
--- controllers/login.js
+++ controllers/login.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 const loginRouter = require('express').Router();
 const User = require('../models/user');
 
+
 loginRouter.post('/', async (request, response) => {
   const { username, password } = request.body;
 
