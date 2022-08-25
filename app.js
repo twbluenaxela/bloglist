ce618f7ecfe141dd42997e9b24270855c3357f0a
Nick Yu <rednaxela64@outlook.com>
2022-08-25 07:55:38 -0700

    add testing router

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ app.use('/api/login', middleware.tokenExtractor, loginRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/blogs', middleware.tokenExtractor, blogsRouter);
 
+if(process.env.NODE_ENV === 'testing') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
